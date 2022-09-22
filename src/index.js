4d2d33242c1e0ed30921d5e07f6e2e2d5be13bc8
Rocio Carle <rociocarle@yahoo.com.ar>
2022-09-22 01:58:40 -0300

    Probando apiKey

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const path = require("path");
+// const fetch = require("node-fetch");
+const pokemon = require ('pokemontcgsdk');
 
 app.listen( PORT , () => {
     console.log('listening on port ' + PORT);
@@ -9,6 +11,14 @@ app.listen( PORT , () => {
 
 app.use(express.static(path.resolve(__dirname, '../public')));
 
+pokemon.configure({apiKey: '6e705804-2537-4c0a-b668-c2ba4008762f'})
+
 app.get('/', (req, res) => {
     res.send('Welcome to my app')
 });
+
+
+pokemon.card.find('base1-4')
+.then(card => {
+    console.log(card.name) // "Charizard"
+})
